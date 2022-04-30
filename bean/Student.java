c016915789f2e7f1cef276d429623270c5a84614
WangLian <2366041297@qq.com>
2022-04-30 18:42:14 +0800

    初始化项目

---
+++ bean/Student.java
@@ -0,0 +1,22 @@
+package bean;
+public class Student{
+   private long ID;
+   private String name;
+
+   public void setID(long ID){
+      this.ID = ID;
+   }
+
+   public long getID(){
+      return ID;
+   }
+
+
+   public void setName(String name){
+      this.name = name;
+   }
+
+   public String getName(){
+      return name;
+   }
+}
---
+++ daoIMP/StudentDAOIMP.java
@@ -0,0 +1,126 @@
+package daoIMP;
+import bean.Student;
+import dao.StudentDAO;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import connection.DataBaseConnection;
+
+public class StudentDAOIMP implements StudentDAO{
+	// 添加操作
+    public void insert(Student s){
+    	String sql = "INSERT INTO student (id, name) values (?,?)";
+	    PreparedStatement pstmt = null;
+	    DataBaseConnection conn = null;
+	    //针对数据库的具体操作
+	    try{
+	        conn = new DataBaseConnection();
+
+	        pstmt = conn.getConnection().prepareStatement(sql);
+	        pstmt.setLong(1,s.getID());
+	        pstmt.setString(2,s.getName());
+
+	        pstmt.executeUpdate();
+	        pstmt.close();
+	        conn.close();
+	    } catch(Exception e){
+
+	    }
+    }
+
+    public void update(Student s){
+	    String sql = "UPDATE student SET name = ? WHERE id = ?";
+	    PreparedStatement pstmt = null;
+	    DataBaseConnection conn = null;
+	    //针对数据库的具体操作
+	    try{
+		    conn = new DataBaseConnection();
+
+		    pstmt = conn.getConnection().prepareStatement(sql);
+		    pstmt.setString(1,s.getName());
+		    pstmt.setLong(2,s.getID());
+
+		    pstmt.executeUpdate();
+		    pstmt.close();
+		    conn.close();
+	    } catch(Exception e){
+
+	    }
+    }
+
+    public void delete(String ID){
+	    String sql = "DELETE FROM student WHERE id = ?";
+	    PreparedStatement pstmt = null;
+	    DataBaseConnection conn = null;
+	    //针对数据库的具体操作
+	    try{
+		    conn = new DataBaseConnection();
+
+		    pstmt = conn.getConnection().prepareStatement(sql);
+		    pstmt.setLong(1,Long.parseLong(ID));
+		    pstmt.executeUpdate();
+		    pstmt.close();
+		    conn.close();
+	    } catch(Exception e){
+
+	    }
+    }
+	    
+    public List findAll(){
+	    String sql = "SELECT * FROM student";
+	    PreparedStatement pstmt = null;
+	    DataBaseConnection conn = null;
+	    List<Map<String,Object>> list = new ArrayList<>();
+	    //针对数据库的具体操作
+	    try{
+		    conn = new DataBaseConnection();
+
+		    pstmt = conn.getConnection().prepareStatement(sql);
+		    ResultSet resultSet = pstmt.executeQuery();
+		    ResultSetMetaData resultSetMetaData = resultSet.getMetaData();    //获得元数据
+		    int columnCount = resultSetMetaData.getColumnCount();   //获得总的列数
+		    while (resultSet.next()) {
+			    Map<String,Object> rsMap = new HashMap<String,Object>();
+			    for (int i = 1; i <= columnCount; i++) {
+			    	//循环每一列，并获取每一列里面的数据
+				    rsMap.put(resultSetMetaData.getColumnName(i),resultSet.getObject(i));
+			    }
+			    list.add(rsMap);
+		    }
+		    pstmt.close();
+		    conn.close();
+	    } catch(Exception e){
+	    }
+	    return list;
+    }
+
+    public Student findByID(long ID){
+	    String sql = "SELECT * FROM student WHERE id = ?";
+	    PreparedStatement pstmt = null;
+	    DataBaseConnection conn = null;
+	    Student student = new Student();
+	    //针对数据库的具体操作
+	    try{
+		    conn = new DataBaseConnection();
+
+		    pstmt = conn.getConnection().prepareStatement(sql);
+		    pstmt.setLong(1,ID);
+		    ResultSet resultSet = pstmt.executeQuery();
+		    while (resultSet.next()) {
+		    	//第一列是计数器 数据从第二列开始
+			    student.setID(resultSet.getLong(2));
+			    student.setName(resultSet.getString(3));
+		    }
+		    pstmt.close();
+		    conn.close();
+
+	    } catch(Exception e){
+
+	    }
+	    return student;
+    }
+
+}
